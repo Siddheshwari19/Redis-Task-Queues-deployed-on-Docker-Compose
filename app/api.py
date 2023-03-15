a2b3e3b55a0b0d5f7e33733f165aa4e4d6cc573a
Siddheshwari Bankar <93153714+Siddheshwari19@users.noreply.github.com>
2023-03-15 12:46:32 -0700

    Add files via upload

---
+++ app/api.py
@@ -0,0 +1,10 @@
+import time
+import requests
+
+def getCatImage(delay):
+
+    time.sleep(delay)
+    url = requests.get(
+        "https://api.thecatapi.com/v1/images/search").json()[0]['url']
+
+    return url
